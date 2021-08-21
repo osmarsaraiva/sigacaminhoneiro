2dc8b9d66d29ce441859c57f3a87c3925a1cfa78
Osmar Saraiva Jr <osmar.saraiva@gmail.com>
2021-08-21 10:18:32 -0300

    primeiro commit

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php 
+
+$nome_sistema = 'Siga Caminhoneiro';
+$url_sistema = 'http://localhost/sigacaminhoneiro/';
+$email_adm = 'osmar.saraiva@gmail.com';
+$nome_admin = 'Osmar saraiva Jr';
+
+
+//DADOS PARA O BANCO DADOS
+$servidor = 'localhost:3304';
+$usuario = 'root';
+$senha = 'usbw';
+$banco = 'sigacaminhoneiro';
+
+ ?>
